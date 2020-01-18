8b6e607ba58522111bdca9ed3474e57223789941
Reuno92 <racinet.renaud@orange.fr>
2020-01-18 13:21:08 +0100

    test(Fancyheading): test title is Hello World

--- tests/unit/fancyHeading.spec.ts
+++ tests/unit/fancyHeading.spec.ts
@@ -6,4 +6,10 @@ describe('Unit test for FancyHeading component', () => {
     const wrapper = mount(FancyHeading);
     expect(wrapper.isVueInstance()).toBeTruthy();
   });
+
+  test('title should be Hello World', () => {
+    const msg = 'Hello world';
+    const wrapper = mount(FancyHeading);
+    expect(wrapper.find('h1').text()).toBe(msg);
+  });
 });
